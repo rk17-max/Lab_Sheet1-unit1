d11b3bd8496d89bceb37cf4e2a7da71730c489a0
rk17-max <143068154+rk17-max@users.noreply.github.com>
2024-11-09 23:24:19 +0530

    Update StaticArray.cpp

--- StaticArray.cpp
+++ StaticArray.cpp
@@ -1,1 +1,46 @@
 
+#include <iostream>
+#include <vector>
+
+class StaticArray {
+private:
+    int* array;
+    int size;
+    int index;
+
+public:
+    StaticArray(int _size) : size(_size), index(0) {
+        array = new int[size];
+    }
+
+    ~StaticArray() {
+        delete[] array;
+    }
+
+    void insert(int value) {
+        if (index < size) {
+            array[index] = value;
+            index++;
+        } else {
+            std::cout << "Array is full." << std::endl;
+        }
+    }
+
+    void deleteValue(int pos) {
+        if (pos >= 0 && pos < index) {
+            for (int i = pos; i < index - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            index--;
+        } else {
+            std::cout << "Index out of range." << std::endl;
+        }
+    }
+
+    void traverse() {
+        for (int i = 0; i < index; i++) {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
